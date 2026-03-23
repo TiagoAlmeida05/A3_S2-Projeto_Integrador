e566cf258f89a4d88b287685d927d1603cd7fdf9
Tiago Almeida <up202303450@up.pt>
2026-03-23 21:22:24 +0000

    Getting the project started

---
+++ backend/database.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+DB_NAME = 'database.db'
+
+def get_db_connection():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS projects (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            description TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    init_db()
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # This is the "app" variable that Uvicorn is looking for!
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
-def read_root():
-    return {"message": "My FastAPI server is successfully running!"}
+async def root():
+    return {"message": "Hello World"}
